29945f4261def9963e0ad0da42c966d859e1e64d
Amanda Pellechia <aepellechia@gmail.com>
2018-02-21 19:54:43 -0500

    Complete chapters 1 & 2 and some of 3

---
+++ kmain.c
@@ -0,0 +1,36 @@
+    #include "io.h"
+
+    /* The I/O ports */
+    #define FB_COMMAND_PORT         0x3D4
+    #define FB_DATA_PORT            0x3D5
+
+    /* The I/O port commands */
+    #define FB_HIGH_BYTE_COMMAND    14
+    #define FB_LOW_BYTE_COMMAND     15
+    #define FB_GREEN     2
+    #define FB_DARK_GREY 8
+
+int kmain(void){
+
+	return(0);
+
+}
+
+int write(char *buf, unsigned int len){
+	fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
+	fb_move_cursor(len);
+}
+
+ void fb_move_cursor(unsigned short pos)
+    {
+        outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+        outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
+        outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+        outb(FB_DATA_PORT,    pos & 0x00FF);
+    }
+
+   void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+    {
+        fb[i] = c;
+        fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F)
+    }
